669264a003c382cf901288ccd183b8b698f54afc
Toryang <toryangchen@outlook.com>
2019-10-16 21:41:41 +0800

    add webpack learn project

---
+++ webpack-sample/src/index.js
@@ -0,0 +1,7 @@
+import Header from './header.js'
+import Sidebar from './sidebar.js'
+import Content from './content.js'
+
+new Header()
+new Sidebar()
+new Content()
---
+++ webpack-sample/src/sidebar.js
@@ -0,0 +1,8 @@
+function SideBar() {
+  var dom = document.getElementById('root')
+  var sidebar = document.createElement('div')
+  sidebar.innerText = 'content'
+  dom.appendChild(sidebar)
+}
+
+export default SideBar
